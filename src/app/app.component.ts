0e140f6606bc8d0dd9cb8a5a405ef1c5adbed10a
wayne byard <user00015@gmail.com>
2025-05-17 18:30:02 -0400

    Finish this tomorrow.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
